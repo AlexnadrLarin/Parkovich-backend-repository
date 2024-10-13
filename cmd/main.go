c830ab965c4d523cf8209afe96a6e44b649e3084
AlexnadrLarin <allarin03@mail.ru>
2024-10-13 21:51:52 +0300

    Add main file

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"go-parkovich/microservices/analytics/cmd"
+	"go-parkovich/microservices/analytics/pkg/proto"
+
+)
+
+func main() {
+	go func() {
+		if err := cmd.Start(); err != nil {
+			log.Fatalf("Ошибка при запуске микросервиса: %v", err)
+		}
+	}()
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure()) 
+	if err != nil {
+		log.Fatalf("Ошибка подключения к микросервису по gRPC: %v", err)
+	} 
+	log.Println("Подключены к gRPC серверу на порту 50051")
+	defer conn.Close()
+
+	grpcClient := userevents.NewUserEventServiceClient(conn)
+
+	_ = grpcClient
+
+	log.Println("Главный сервер запущен на порту :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Ошибка запуска главного сервера: %v", err)
+	}
+}
