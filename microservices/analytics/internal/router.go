f6dd47c26ff7d84967917513bfacfd0d05c43d9c
AlexnadrLarin <allarin03@mail.ru>
2024-10-20 21:42:52 +0300

    Fix router

--- microservices/analytics/internal/router.go
+++ microservices/analytics/internal/router.go
@@ -1,25 +1,21 @@
 package internal
 
 import (
-    "net/http"
-	
     "go-parkovich/microservices/analytics/internal/api"
     "go-parkovich/microservices/analytics/internal/database"
+    "go-parkovich/microservices/analytics/internal/middleware"
 
     "github.com/gorilla/mux"
+
 )
 
 func SetupRouter(repo *database.UserEventsRepository) *mux.Router {
     r := mux.NewRouter()
 
-    r.HandleFunc("/api/v1/user-action", api.HandleUserAction(repo)).Methods(http.MethodPost)
-
-    r.HandleFunc("/api/v1/user-actions", api.GetAllUserActions(repo)).Methods(http.MethodGet)
-
-    r.HandleFunc("/api/v1/user-actions/{user_id}", api.GetUserActions(repo)).Methods(http.MethodGet)
-
-    r.HandleFunc("/api/v1/action-and-device-counts", api.GetActionAndDeviceCounts(repo)).Methods(http.MethodGet)
-
+    r.HandleFunc("/api/v1/user-action", middleware.UserEventsValidationMiddleware(api.HandleUserAction(repo))).Methods("POST")
+    r.HandleFunc("/api/v1/user-actions", api.GetAllUserActions(repo)).Methods("GET")
+    r.HandleFunc("/api/v1/users-actions/", api.GetUserActions(repo)).Methods("GET")
+    r.HandleFunc("/api/v1/action-and-device-counts", api.GetActionAndDeviceCounts(repo)).Methods("GET")
 
     return r
 }
